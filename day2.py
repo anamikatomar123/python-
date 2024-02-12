3bae85f51a5efe9152c5678c6151efaa54ded032
anamikakacher <anamikakacher171@gmail.com>
2024-02-12 19:56:22 +0530

    added

---
+++ day2.py
@@ -0,0 +1,32 @@
+print("hello")
+
+a = 10
+print(a)
+print(type(a))
+
+b = 20.5
+print(b)
+print(type(b))
+
+c = True
+print(c)
+print(type(c))
+
+
+d = "chinmay"
+print(d)
+print(type(d))
+
+print(6 > 3)
+print(6 < 3)
+print(6 >= 3)
+print(6 <= 3)
+print(6 >= 6)
+print(6 <= 6)
+print(6 != 6)
+print(7 == 7)
+
+print(5 == 5 and 6 == 6)
+print(7 == 8 and 9 == 9)
+print(6 == 6 and 8 == 9)
+print(8 == 9 and 9 == 8)
