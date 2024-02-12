b1f9a05a0516954d42b15dbf1ba09a9fc482ddb7
anamikakacher <anamikakacher171@gmail.com>
2024-02-12 19:54:40 +0530

    added

---
+++ day1.py
@@ -0,0 +1,61 @@
+x = 10
+print(x)
+x = 100
+print(x)
+
+
+s = 9 
+t = 3
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+
+q = 8 
+r = 4
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+
+Calculator(12,3)
+Calculator(12,4)
+
+
+
+def addA():
+    print(9+9)
+addA()
+addA()
+addA()
+addA()
+
+
+
+def addB(x,y):
+    print(x+y)
+addB(12,3)
+addB(1,3)
+addB(11,3)
+
+
+
+def addC(x,y):
+    return x + y
+e = addC(24,5)
+print(e)
+print(e + e)
+print(e * e)
